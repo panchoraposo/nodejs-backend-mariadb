94b64a6a66898f36c9fa08e29ab8388702997eeb
Francisco Raposo <pancho.raposo@gmail.com>
2023-12-22 15:34:26 -0300

    port

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ app.use('/sensor', require('./event/event.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 3000) : 3000;
 app.listen(port, () => console.log('Server listening on port ' + port));
